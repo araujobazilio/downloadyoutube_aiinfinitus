9772535d828e0e01d42161bd83eebf18d2976265
araujobazilio <rafael.araujobazilio@gmail.com>
2024-10-27 22:04:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import yt_dlp
 import os
-import tkinter as tk
-from tkinter import filedialog
 
 # Função para exibir o progresso
 def progresso(status):
@@ -32,17 +30,13 @@ def baixar_video(url, diretorio):
     except Exception as e:
         st.error(f"Ocorreu um erro: {e}")
 
-# Função para abrir a caixa de diálogo para selecionar o diretório
-def selecionar_diretorio():
-    root = tk.Tk()
-    root.withdraw()  # Esconde a janela principal do Tkinter
-    diretorio = filedialog.askdirectory(title="Selecione o diretório para salvar o vídeo")
-    return diretorio
-
 # Interface do Streamlit
 st.title("Downloader de Vídeo do YouTube")
 st.text("Digite a URL do vídeo do YouTube abaixo:")
 url = st.text_input("URL do vídeo do YouTube:")
+
+# Campo para inserir o diretório
+diretorio = st.text_input("Caminho do diretório para salvar o vídeo:", os.path.join(os.path.expanduser("~"), "Downloads"))
 
 # Inicializa a barra de progresso e o texto de progresso
 if 'progress_bar' not in st.session_state:
@@ -50,20 +44,11 @@ if 'progress_bar' not in st.session_state:
 if 'progress_text' not in st.session_state:
     st.session_state.progress_text = st.empty()
 
-# Botão para selecionar o diretório
-if st.button("Selecionar Diretório"):
-    diretorio = selecionar_diretorio()
-    if diretorio:
-        st.session_state.selected_directory = diretorio
-        st.success(f"Diretório selecionado: {diretorio}")
-    else:
-        st.warning("Nenhum diretório selecionado.")
-
 if st.button("Baixar Vídeo"):
-    if url and 'selected_directory' in st.session_state:
+    if url and diretorio:
         st.write("Iniciando o processo de download...")
         st.session_state.progress_bar.progress(0)  # Reseta a barra de progresso
         st.session_state.progress_text.text("Progresso: 0%")  # Reseta o texto de progresso
-        baixar_video(url, st.session_state.selected_directory)
+        baixar_video(url, diretorio)
     else:
-        st.warning("Por favor, insira uma URL válida e selecione um diretório.")
+        st.warning("Por favor, insira uma URL válida e um caminho de diretório.")
